1c7063692c00a105d889e5ea6e4bc24ffe78d82b
Aryan Kumar <aryan1732003@gmail.com>
2026-02-03 15:02:56 +0530

    testing ci

--- scripts/promote_model_to_prod.py
+++ scripts/promote_model_to_prod.py
@@ -3,12 +3,10 @@ import dagshub
 import json
 from mlflow import MlflowClient
 
-dagshub.init(repo_owner='himanshu1703', 
-             repo_name='swiggy-delivery-time-prediction', 
-             mlflow=True)
+dagshub.init(repo_owner='constantaryan', repo_name='swiggy-delivery-time-prediction', mlflow=True)
 
 # set the mlflow tracking server
-mlflow.set_tracking_uri("https://dagshub.com/himanshu1703/swiggy-delivery-time-prediction.mlflow")
+mlflow.set_tracking_uri("https://dagshub.com/constantaryan/swiggy-delivery-time-prediction.mlflow")
 
 def load_model_information(file_path):
     with open(file_path) as f:
